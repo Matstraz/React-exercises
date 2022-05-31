5dfa5b7e92d7acabe8df4b96d1eac0aff2ac635d
Matteo <99515747+Matstraz@users.noreply.github.com>
2022-05-31 13:36:13 +0200

    feat: fix prop ex 2

--- src/welcome.js
+++ src/welcome.js
@@ -6,15 +6,15 @@ export class Welcome extends React.Component{
             <div>
                 <p>Welcome, {this.props.name}</p>
                 <p>Your age is {this.props.age}</p>
+                {this.props.elemento}
             </div> 
         ) 
+    }
+    // Can you set a default value for the name prop?
+    static defaultProps = {
+        name: 'Nino',
     }
 }
 
 //What happens if no name prop is passed to the Welcome component?
 //La proprietà non verrà renderizzata, come se {this.props.name} non fosse stato scritto.
-
-// Can you set a default value for the name prop?
-Welcome.defaultProps = {    
-    name: "Nino"
-  }
