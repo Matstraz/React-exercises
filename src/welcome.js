afc3d616e1e7124e503f4618d6e1e3cea329dd1f
Matteo <99515747+Matstraz@users.noreply.github.com>
2022-05-31 12:55:51 +0200

    feat: add props ex 2

--- src/welcome.js
+++ src/welcome.js
@@ -5,3 +5,11 @@ export class Welcome extends React.Component{
         return <p>Welcome, {this.props.name}!</p>
     }
 }
+
+//What happens if no name prop is passed to the Welcome component?
+//La proprietà non verrà renderizzata, come se {this.props.name} non fosse stato scritto.
+
+// Can you set a default value for the name prop?
+Welcome.defaultProps = {    
+    name: "Nino"
+  }
