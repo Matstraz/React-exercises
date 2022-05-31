b0ed77fc6a0ef0bfdbea769797f931b1da24b871
Matteo <99515747+Matstraz@users.noreply.github.com>
2022-05-31 12:46:07 +0200

    feat: add ex 1

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Hello } from "./hello";
+import { Welcome } from "./welcome";
 
 export class App extends React.Component{
     render(){
         return (
             <div>
                 <Hello/>
+                <Welcome name="Nino" />
             </div>
         )
     }
