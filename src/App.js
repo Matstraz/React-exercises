915e8a4e94b2bb8597167f18c3554447592a31c2
Matteo <99515747+Matstraz@users.noreply.github.com>
2022-06-01 20:03:53 +0200

    feat: add state ex 1

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Counter } from "./counter";
+
+export class App extends React.Component{
+    render(){
+        return (
+            <div>
+                <Counter />
+            </div>
+        )
+    }
+}
+
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom';
+import { App } from './App';
+
+
+
+const helloWorldElement = <App />
+const root = document.getElementById('root')
+
+ReactDOM.render(helloWorldElement, root)
