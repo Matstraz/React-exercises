7c28cb3ade233239c1f595704118113aa0882649
Matteo <99515747+Matstraz@users.noreply.github.com>
2022-06-01 23:22:27 +0200

    feat: add lifecycle ex 1

--- src/counter.js
+++ src/counter.js
@@ -33,9 +33,3 @@ Counter.defaultProps = {
     increment: 2,
     timeOut: 250
 }
-
-
-//Reset the counter to the initial value received as a prop when the value is greater than ten times that initial value.
-
-//When calling setState to increment the counter, should the parameter be a function or an object? Why?
-//Il parametro dovrebbe essere un oggetto perchè è ciò che rappresenta l'attuale state del componente e che andrà ad aggiornare il precedente.
