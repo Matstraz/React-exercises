44b1f9c10819fe2ff1cbb54b9877c28709049549
Matteo <99515747+Matstraz@users.noreply.github.com>
2022-06-01 21:51:17 +0200

    feat: add state ex 5

--- src/counter.js
+++ src/counter.js
@@ -16,15 +16,26 @@ export class Counter extends React.Component{
     }
 
     render(){
-        return <CounterDisplay count={this.state.count}/>
+        return (
+            <div>
+                {this.state.count < this.props.initialValue * 10
+                ?  <CounterDisplay count={this.state.count}/>
+                :  <CounterDisplay count={this.props.initialValue} />
+            } 
+            
+            </div>
+        )
     }
 }
 
 Counter.defaultProps = {
     initialValue: 10,
     increment: 2,
-    timeOut: 700
+    timeOut: 250
 }
+
+
+//Reset the counter to the initial value received as a prop when the value is greater than ten times that initial value.
 
 //When calling setState to increment the counter, should the parameter be a function or an object? Why?
 //Il parametro dovrebbe essere un oggetto perchè è ciò che rappresenta l'attuale state del componente e che andrà ad aggiornare il precedente.
