087a7bf5305f6ba3df6e5c5fff1293b2d048264c
Matteo <99515747+Matstraz@users.noreply.github.com>
2022-06-06 21:01:35 +0200

    feat: add forms ex 5

---
+++ src/UncontrolledLogin.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { createRef } from "react";
+
+export class UncontrolledLogin extends React.Component{
+    
+    _formRef = createRef()
+
+    handleFormSubmit = (event) =>{
+      event.preventDefault();
+        const username = event.target.elements.username.value
+        const password = event.target.elements.password.value
+        const remember = event.target.elements.remember.checked
+
+        console.log({username, password, remember})
+    }
+
+    handleAutofillForm = () => {
+        this._formRef.current.elements.username.value = "Nino"
+        this._formRef.current.elements.password.value = "01234"
+        this._formRef.current.elements.remember.checked = true
+    }
+
+    render(){
+        return (
+            <div>
+                <h3>Uncontrolled Login</h3>
+                <form ref={this._formRef} onSubmit={this.handleFormSubmit}>
+                    <input name="username" type="text" />
+                    <input name="password" type="password" />
+                    <input name="remember" type="checkbox" />
+
+                    <button>Submit</button>
+                    <button type="reset">Reset</button>
+                    <button type="button" onClick={this.handleAutofillForm}>Autofill</button>
+                </form>
+            </div>
+        )
+    }
+}
