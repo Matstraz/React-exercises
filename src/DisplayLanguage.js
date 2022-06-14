e8c7ca9c754a2b4afa6184a76db5ce447f0a4c9e
Matteo <99515747+Matstraz@users.noreply.github.com>
2022-06-14 11:26:19 +0200

    feat: add context ex 2

--- src/DisplayLanguage.js
+++ src/DisplayLanguage.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { LanguageContext } from "./LanguageContext";
 
-export class DisplayLanguage extends React.Component{
-    render(){
-        return
-    }
+const Strings = {
+    en: {
+      currentLanguage: "The selected language is: ",
+    },
+    it: {
+        currentLanguage: "La lingua selezionata è: ",
+    },
+  };
+
+export class DisplayLanguage extends React.Component {
+  render() {
+    return (
+      <div>
+        <LanguageContext.Consumer>
+          {(language) => {
+            return <h1> {Strings[language].currentLanguage}{language}</h1>;
+          }}
+        </LanguageContext.Consumer>
+      </div>
+    );
+  }
 }
