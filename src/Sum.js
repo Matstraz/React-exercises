0103dce0eb7349bf27a01a6b03e527d761ed13dc
Matteo <99515747+Matstraz@users.noreply.github.com>
2022-06-14 17:00:33 +0200

    feat: add functionComponents ex 3

--- src/Sum.js
+++ src/Sum.js
@@ -1,3 +1,6 @@
-export function Sum ({numbers}){
+export function Sum ({numbers=[1, 2, 3, 4, 5, 6]}){
     return <h1>{numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0)}</h1>
 }
+
+//What happens if the numbers prop of the Sum component is not set?
+//Non viene renderizzato nulla
