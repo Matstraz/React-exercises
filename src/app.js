0b8336da2ee6fc4bc234702015b8b28ca5efa1c1
Matteo <99515747+Matstraz@users.noreply.github.com>
2022-06-14 11:14:48 +0200

    feat: add context ex 1

--- src/app.js
+++ src/app.js
@@ -1,11 +1,26 @@
 import React from "react";
-import { Hello } from "./hello";
+import { DisplayLanguage } from "./DisplayLanguage";
+import { LanguageContext } from "./LanguageContext";
 
 export class App extends React.Component{
+  state = {
+    language: 'en'
+  }
+  
+  handleLanguageChange = (event) => {
+    this.setState({ language: event.target.value });
+  };
+  
     render(){
         return (
             <div>
-                <Hello/>
+              <select value={this.state.language} onChange={this.handleLanguageChange}>
+                <option value='en'>English</option>
+                <option value='it'>Italian</option>
+              </select>
+              <LanguageContext.Provider value={this.state.language}>
+                <DisplayLanguage/>
+              </LanguageContext.Provider>
             </div>
         )
     }
