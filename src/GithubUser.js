83cbe6fff18037d16d79e61c21dd6fa2071436e9
Matteo <99515747+Matstraz@users.noreply.github.com>
2022-06-17 20:50:47 +0200

    feat: add customHokks ex 4

--- src/GithubUser.js
+++ src/GithubUser.js
@@ -1,15 +1,19 @@
 import { useGithubUser } from "./useGithubUser"
 
 export function GithubUser({username}){
-    const {userData} = useGithubUser(username)
+    const {userData, loading, error} = useGithubUser(username)
 
    
     return(
         <div>
-            <h1>Github Data</h1>
-            <h3>ID: {userData.id}</h3>
-            <h3>Name: {userData.name}</h3>
-            <h3>Followers: {userData.followers}</h3>
+            {loading && <h1>Loading</h1>}
+            {userData && <h1>Github Data</h1>}
+            {userData && <ul><li>ID: {userData.id}</li>
+            <li>Name: {userData.name}</li>
+            <li>Followers: {userData.followers}</li></ul>}
+            {error && <h2>An error has occurred</h2>}
+          
+           
         </div>
     )
 }
--- src/useGithubUser.js
+++ src/useGithubUser.js
@@ -1,20 +1,30 @@
-/* Extract the logic to fetch a Github user's data from the GithubUser
- component from useEffect 03 into a custom hook called useGithubUser. */
-
 import { useState, useEffect } from "react"
 
  export function useGithubUser(username){
 
     const [hub, setHub] = useState(username)
+    const [loading, setLoading] = useState (false)
+    const [error, setError] = useState(null)
 
     useEffect(()=> {
+        setLoading(true)
+        setError(null)
         fetch(`https://api.github.com/users/${username}` )
-        .then((response) => response.json())
+        .then((response) =>{
+            if(response.status !== 200){
+                setError(new Error('User not found'))
+            }
+            return response.json()})
         .then((data)=> setHub(data))
+        .catch(error => setError(error))
+        .finally(()=> setLoading(false))
     }, [username])
     
+
     return{
-        userData: hub
+        userData: hub,
+        loading: loading,
+        error: error
     }
 
- }
+}
