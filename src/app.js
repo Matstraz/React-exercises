5a5090282f1cbfe4f863d8802982f1304b604687
Matteo <99515747+Matstraz@users.noreply.github.com>
2022-06-19 00:13:53 +0200

    feat: add reactRouter ex 5

--- src/app.js
+++ src/app.js
@@ -9,12 +9,14 @@ export function App(){
         return (
             <div>
                 <h2>Use links to navigate within the App</h2>
-                <h3><Link to="/">Home</Link> | <Link to="/counter">Counter</Link> | <Link to="/users/">Github Users</Link></h3>
+                <h3><Link to="/">Home</Link> | <Link to="/counter">Counter</Link> | <Link to="users/">Github Users</Link></h3>
                 <hr/>
                 <Routes>
                     <Route path="/" element={<Welcome name={'Nino'}/>}/>
-                    <Route path="/counter" element={<Counter />}/>
-                    <Route path="/users/:username" element={<ShowGithubUser />}/>
+                    <Route path="counter" element={<Counter />}/>
+                    <Route path="users/:username" element={<ShowGithubUser />}/>
+                    <Route path="users/" element={<ShowGithubUser />}/>          {/* Default username value */}
+                    <Route path="*" element={<h2>Route not Found, please go back <Link to="/">Home</Link></h2>}/>
                 </Routes>
                 
            
@@ -22,3 +24,5 @@ export function App(){
         )
     
 }
+
+/* Add a Not Found route that renders when a user navigates to a path that does not exist. */
