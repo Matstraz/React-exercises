fcd5acfbc903d56406db3944836bd209d8bb2161
Matteo <99515747+Matstraz@users.noreply.github.com>
2022-06-18 23:58:22 +0200

    feat: add reactRouter ex 4

--- src/ShowGithubUser.js
+++ src/ShowGithubUser.js
@@ -4,12 +4,7 @@ import { GithubUser } from "./GithubUser";
 
 export function ShowGithubUser(){
 
-    const {username} = useParams()
+    const {username = 'Matstraz'} = useParams()
 
     return <GithubUser username={username}/>
 }
-
-
-/*Add a new Route to the users/:username path that renders a ShowGithubUser 
-component that receives the username as a path parameter and renders 
-the GithubUser component from useEffect 03 by passing it the received username.*/
--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Link } from "react-router-dom";
 import { Counter } from "./Counter";
 import { ShowGithubUser } from "./ShowGithubUser";
 import { Welcome } from "./welcome";
@@ -8,6 +8,9 @@ import { Welcome } from "./welcome";
 export function App(){
         return (
             <div>
+                <h2>Use links to navigate within the App</h2>
+                <h3><Link to="/">Home</Link> | <Link to="/counter">Counter</Link> | <Link to="/users/">Github Users</Link></h3>
+                <hr/>
                 <Routes>
                     <Route path="/" element={<Welcome name={'Nino'}/>}/>
                     <Route path="/counter" element={<Counter />}/>
