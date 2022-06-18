b23052d243ebd3741bb4ff69751f9947ad6dafd4
Matteo <99515747+Matstraz@users.noreply.github.com>
2022-06-19 01:04:55 +0200

    feat: add reactRouter ex 6

---
+++ src/GithubUserList.js
@@ -0,0 +1,34 @@
+import { useState } from "react"
+import { Link } from "react-router-dom";
+import { Outlet } from "react-router";
+
+
+
+export function GithubUserList(){
+
+    const [newUser, setNewUser] = useState([])
+    const [usernames, setUsernames] = useState(['Gino', 'Nando', 'Michele'])
+
+    function handleTypeUsername(event){
+        setNewUser(event.target.value)
+    }
+
+    function handleAddUsername(){
+        setUsernames((usernames) => usernames.concat(newUser))
+    }
+
+    return(
+        <div>
+            <ul>{usernames.map((name, index) => <li key={name + index}><Link to="users/">{name}</Link></li>)}</ul>
+            <input type="text" value={newUser} onChange={handleTypeUsername}></input>
+            <button onClick={handleAddUsername}>Add User</button>
+            <Outlet/>
+        </div>
+    )
+}
+
+/* Add a Route to the users path that shows the GihubUserList component from useEffect 04.
+ Modify it so that instead of showing the GithubUser component for each username entered, 
+ it shows a link to a nested route that shows the ShowGithubUser component. In doing so,
+  remove the /users/:username route from the App component, and add a new nested route within
+  the /users route. */
--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Routes, Link } from "react-router-dom";
 import { Counter } from "./Counter";
+import { GithubUserList } from "./GithubUserList";
 import { ShowGithubUser } from "./ShowGithubUser";
 import { Welcome } from "./welcome";
 
@@ -9,13 +10,14 @@ export function App(){
         return (
             <div>
                 <h2>Use links to navigate within the App</h2>
-                <h3><Link to="/">Home</Link> | <Link to="/counter">Counter</Link> | <Link to="users/">Github Users</Link></h3>
+                <h3><Link to="/">Home</Link> | <Link to="/counter">Counter</Link> | <Link to="userslist">Github Users List</Link></h3>
                 <hr/>
                 <Routes>
                     <Route path="/" element={<Welcome name={'Nino'}/>}/>
                     <Route path="counter" element={<Counter />}/>
-                    <Route path="users/:username" element={<ShowGithubUser />}/>
-                    <Route path="users/" element={<ShowGithubUser />}/>          {/* Default username value */}
+                    <Route path="userslist" element={<GithubUserList />}>
+                        <Route path="users/" element={<ShowGithubUser />}/>
+                    </Route>
                     <Route path="*" element={<h2>Route not Found, please go back <Link to="/">Home</Link></h2>}/>
                 </Routes>
                 
@@ -25,4 +27,9 @@ export function App(){
     
 }
 
-/* Add a Not Found route that renders when a user navigates to a path that does not exist. */
+
+/* Add a Route to the users path that shows the GihubUserList component from useEffect 04.
+ Modify it so that instead of showing the GithubUser component for each username entered, 
+ it shows a link to a nested route that shows the ShowGithubUser component. In doing so,
+  remove the /users/:username route from the App component, and add a new nested route within
+  the /users route. */
