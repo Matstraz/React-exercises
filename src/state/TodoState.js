1ade6428145d9e4f6e7146f5a04578d87e465a46
Matteo <99515747+Matstraz@users.noreply.github.com>
2022-06-27 18:17:32 +0200

    feat: add redux ex 3

---
+++ src/state/TodoState.js
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit";   
+
+export const todoList = createSlice({  
+    name: 'todoList',
+    initialState: [],
+    reducers: {
+        add: (state, action) =>  [...state, action.payload], // {state.push(action.payload)} Può essere scritto anche così perchè si sta mutando lo state pushando qualcosa, ciò è posssibile grazie ad IMMER.
+        remove: (state, action) => state.filter((item)=> item !== action.payload),
+        edit: (state, action) => state.map(item => {                                              
+            if (item.id === action.payload.id){
+                return {...item, ...action.payload.data}
+            }
+
+            return item}) 
+    }
+})
