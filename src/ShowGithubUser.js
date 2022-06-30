94eca46e2e8ff45bc6e0d85447a4314ee4bef724
Matteo <99515747+Matstraz@users.noreply.github.com>
2022-06-30 22:23:23 +0200

    feat: as previous commit, edit: fix fetching from github userlists EX 6

--- src/ShowGithubUser.js
+++ src/ShowGithubUser.js
@@ -1,10 +1,8 @@
-import { useParams} from "react-router";
+import { useParams } from "react-router";
 import { GithubUser } from "./GithubUser";
 
+export function ShowGithubUser() {
+  const { username = "Matstraz" } = useParams();
 
-export function ShowGithubUser(){
-
-    const {username = 'Matstraz'} = useParams()
-
-    return <GithubUser username={username}/>
+  return <GithubUser username={username} />;
 }
